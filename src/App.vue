1fb587228d56c8fb9c063327ba090893c5d76967
Darina <dajtbaeva@gmail.com>
2023-04-23 23:36:37 +0600

    upd

---
+++ src/App.vue
@@ -0,0 +1,48 @@
+<template>
+  <VApp>
+    <NavBar />
+    <VMain>
+      <VContainer fluid>
+        <!-- <template v-slot:activator="{ on }">
+          <VTextField
+            v-model="dateFormatted"
+            label="Date"
+            hint="MM/DD/YYYY format"
+            persistent-hint
+            prepend-icon="event"
+            @blur="date = parseDate(dateFormatted)"
+            v-on="on"
+          ></VTextField>
+        </template> -->
+        <datepicker v-model="selected" />
+        <VRow dense>
+          {{ selected }}
+          <VCol v-for="n in 4" :key="n" cols="12">
+            <VCard
+              :title="`Content ${n}`"
+              :subtitle="`Subtitle for Content ${n}`"
+              text="Lorem ipsum dolor sit amet consectetur, adipisicing elit.?"
+            ></VCard>
+          </VCol>
+        </VRow>
+      </VContainer>
+    </VMain>
+  </VApp>
+</template>
+
+<script>
+import NavBar from "./components/NavBar.vue";
+import { ref } from "vue";
+import Datepicker from "vue3-datepicker";
+
+export default {
+  components: {
+    NavBar,
+    Datepicker,
+  },
+  setup() {
+    const selected = ref(new Date());
+    return { selected };
+  },
+};
+</script>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-app-bar color="#fff">
+    <v-img
+      src="https://www.nationalbank.kz/site/img/logo/logo-abbr-en.svg?v=1.2"
+      height="30"
+      max-width="80"
+      style="margin-left: 50px"
+    ></v-img>
+    <v-app-bar-title>National Bank of Kazakhstan</v-app-bar-title>
+
+    <v-spacer></v-spacer>
+
+    <v-btn icon>
+      <v-icon>mdi-heart</v-icon>
+    </v-btn>
+  </v-app-bar>
+</template>
