4b2853d6b84b9c4b97fa4dfed335ff02f6e75058
Darina <dajtbaeva@gmail.com>
2023-05-04 22:16:08 +0600

    upd

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -4,6 +4,7 @@ import "vuetify/styles";
 
 // Vuetify
 import { createVuetify } from "vuetify";
+import * as components from "vuetify/components"
 
-export default createVuetify();
+export default createVuetify({components});
 // https://vuetifyjs.com/en/introduction/why-vuetify/#feature-guides
